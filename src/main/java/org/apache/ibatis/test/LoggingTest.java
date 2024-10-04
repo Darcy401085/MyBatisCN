acd9919cf587b7aad3d975e139d401ad4bb59b71
DarcyYan <571266785@qq.com>
2024-10-04 08:15:53 +0800

    问题：code format； 原因：code format； 修改：修改代码；

--- src/main/java/org/apache/ibatis/test/LoggingTest.java
+++ src/main/java/org/apache/ibatis/test/LoggingTest.java
@@ -1,7 +1,9 @@
-package org.apache.ibatis.test;/** 
+package org.apache.ibatis.test;
+
+/**
  * @author : Darcy
  * @date 2023/2/26
  */
 
-    public class LoggingTest {
+public class LoggingTest {
 }
